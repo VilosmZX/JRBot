c806975a0dba00bad3b8c4031bff54cfa3127a3c
VilosmZX <70438760+VilosmZX@users.noreply.github.com>
2023-09-12 10:22:49 +0000

    Update on_ready events

--- src/cogs/listeners.py
+++ src/cogs/listeners.py
@@ -1,16 +1,20 @@
 import discord
-import os 
+import os
 from discord.ext import commands
 from discord import app_commands
+from web.keep_alive import keep_alive
+
 
 class Listeners(commands.Cog):
-    
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
 
-    @commands.Cog.listener(name='on_ready')
-    async def on_ready(self):
-        print('Bot is online!')
+  def __init__(self, bot: commands.Bot):
+    self.bot = bot
+
+  @commands.Cog.listener(name='on_ready')
+  async def on_ready(self):
+    keep_alive()
+    print('Bot is online!')
+
 
 async def setup(bot: commands.Bot):
-    await bot.add_cog(Listeners(bot))
+  await bot.add_cog(Listeners(bot))
